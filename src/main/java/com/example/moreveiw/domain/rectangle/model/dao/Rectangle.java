546be79cb0dd475d9952f418984e1fa4320fe349
jgmoon <6813821@tukorea.ac.kr>
2024-04-23 01:22:51 +0900

    feat: 사각형 엔티티 추가 (#1)

---
+++ src/main/java/com/example/moreveiw/domain/rectangle/model/dao/Rectangle.java
@@ -0,0 +1,31 @@
+package com.example.moreveiw.domain.rectangle.model.dao;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "rectangle")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rectangle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long projectId;
+
+    private String x;
+    private String y;
+
+    private float width;
+    private float height;
+
+    private String color;
+
+}
