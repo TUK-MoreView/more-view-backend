f03548d87360b8efa301ac8bd3768d6841e2d25d
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-06-14 22:14:03 +0900

    S3 연결

---
+++ src/main/java/com/example/moreveiw/domain/S3/controller/S3Controller.java
@@ -0,0 +1,28 @@
+package com.example.moreveiw.domain.S3.controller;
+
+import com.example.moreveiw.domain.S3.model.dto.S3UrlResponse;
+import com.example.moreveiw.domain.S3.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class S3Controller {
+    private final S3Service imageService;
+
+    @PostMapping(value = "/file",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public S3UrlResponse uploadImage(@RequestBody MultipartFile file) throws IOException {
+        return imageService.uploadImage(file);
+    }
+
+    @DeleteMapping("/file")
+    public boolean deleteImage(@RequestParam String fileUrl) {
+        return imageService.deleteImage(fileUrl);
+    }
+}
