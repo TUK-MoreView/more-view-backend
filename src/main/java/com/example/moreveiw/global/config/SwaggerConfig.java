6ea5ea297cd4bd770d68f422bfc7b93fd2f5acec
김람운 <110807323+ramun2@users.noreply.github.com>
2024-06-24 03:28:47 +0900

    [우진] swagger 오류 fix

--- src/main/java/com/example/moreveiw/global/config/SwaggerConfig.java
+++ src/main/java/com/example/moreveiw/global/config/SwaggerConfig.java
@@ -1,25 +1,31 @@
 package com.example.moreveiw.global.config;
 
-import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SwaggerConfig {
-
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
-    }
-
-    private Info apiInfo() {
-        return new Info()
-                .title("Moreview API 문서")
-                .description("Moreview API 문서입니다.")
-                .version("1.0.0");
+    public OpenAPI customOpenAPI() {
+        List<Server> objects = new ArrayList<>();
+        objects.add(new Server().url("/"));
+        return new OpenAPI().servers(objects)
+                .info(new Info()
+                        .title("API")
+                        .version("1.0")
+                        .description("Base URL: /"));
     }
 }
