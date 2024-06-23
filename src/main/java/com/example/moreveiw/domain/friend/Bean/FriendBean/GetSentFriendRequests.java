a6e657dbca24b09b301ea0c003c7fecf7989c4f7
김람운 <110807323+ramun2@users.noreply.github.com>
2024-06-24 02:44:24 +0900

    보낸 친구 요청 목록 조회 구현

---
+++ src/main/java/com/example/moreveiw/domain/friend/Bean/FriendBean/GetSentFriendRequests.java
@@ -0,0 +1,34 @@
+package com.example.moreveiw.domain.friend.Bean.FriendBean;
+
+import com.example.moreveiw.domain.friend.Bean.SmallBean.FriendBean.FriendRequestListSmallBean;
+import com.example.moreveiw.domain.friend.Bean.SmallBean.UserBean.UserGetByIdSmallBean;
+import com.example.moreveiw.domain.friend.Model.DAO.FriendRequestDAO;
+import com.example.moreveiw.domain.friend.Model.DTO.Friend.Response.FriendRequestListResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class GetSentFriendRequests {
+    private final FriendRequestListSmallBean friendRequestListSmallBean;
+    private final UserGetByIdSmallBean userGetByIdSmallBean;
+    
+    public List<FriendRequestListResponseDTO> exec(Long userId, Integer page, Integer size) {
+        List<FriendRequestDAO> friendRequestDAOS = friendRequestListSmallBean.exec(userId, page, size);
+
+        
+        return friendRequestDAOS.stream()
+                .map(friendRequestDAO -> FriendRequestListResponseDTO.builder()
+                        .friendId(friendRequestDAO.getFriendId())
+                        .friendEmail(userGetByIdSmallBean.exec(friendRequestDAO.getFriendId()).getEmail())
+                        .friendName(userGetByIdSmallBean.exec(friendRequestDAO.getFriendId()).getName())
+                        .friendStatus(friendRequestDAO.getState())
+                        .createdAt(friendRequestDAO.getCreatedAt())
+                        .build())
+                .collect(Collectors.toList());
+
+    }
+}
