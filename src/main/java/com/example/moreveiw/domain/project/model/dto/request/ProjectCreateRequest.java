745abc328c1a71cbaa9fe322594173aed5319463
jgmoon <6813821@tukorea.ac.kr>
2024-06-29 19:08:51 +0900

    chore: dto 디렉토리 구조 변경 (#54)
    dto -> request, response 분리

--- src/main/java/com/example/moreveiw/domain/project/model/dto/request/ProjectCreateRequest.java
+++ src/main/java/com/example/moreveiw/domain/project/model/dto/request/ProjectCreateRequest.java
@@ -1,4 +1,4 @@
-package com.example.moreveiw.domain.project.model.dto;
+package com.example.moreveiw.domain.project.model.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
