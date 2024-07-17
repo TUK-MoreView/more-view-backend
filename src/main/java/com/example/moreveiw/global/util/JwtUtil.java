f008b5987eaccd429f92bd3f9afc16181d51b0b7
jgmoon <6813821@tukorea.ac.kr>
2024-07-18 01:50:13 +0900

    feat: username으로 조회 (#68)

--- src/main/java/com/example/moreveiw/global/util/JwtUtil.java
+++ src/main/java/com/example/moreveiw/global/util/JwtUtil.java
@@ -14,7 +14,7 @@ public class JwtUtil {
 
     private SecretKey secretKey;
 
-    public JwtUtil(@Value("${spring.jwt.secret}")String secret) {
+    public JwtUtil(@Value("${jwt.secret}")String secret) {
 
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
