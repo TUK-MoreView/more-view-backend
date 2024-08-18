37107ea8423b303a38e8dcccb7463bafd96f3ee7
woojin065 <123820750+woojin065@users.noreply.github.com>
2024-08-18 15:18:07 +0900

    s3 오류 수정

--- src/main/java/com/example/moreveiw/domain/S3/controller/S3Controller.java
+++ src/main/java/com/example/moreveiw/domain/S3/controller/S3Controller.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 @Tag(name = "S3 Controller", description = "S3 관련 API")
